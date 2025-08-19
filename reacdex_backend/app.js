55b5b9706d52102f91aac46e4690b2ef4b825de0
Atos Cunha <atos.silvacunha@gmail.com>
2025-08-18 21:38:47 -0300

    Dev verificarions
    Verificações das alterações

--- reacdex_backend/app.js
+++ reacdex_backend/app.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const route_home = require("./routes/main");
 const route_fav = require("./routes/favs");
 const route_img = require("./routes/img");
-// const route_update = require("./route/update");
 
 
 const app = express();
@@ -14,10 +13,6 @@ app.use(cors({origin: "*"}));
 
 app.use('/home', route_home);
 app.use('/fav', route_fav);
-// app.use('/update', route_update);
-
-// app.use('/img', route_img);
-// Servir imagens diretamente (caso queira acessar /img/001.png)
 app.use("/img", express.static(path.join(__dirname, "img")));
 
 const port = 8000;
