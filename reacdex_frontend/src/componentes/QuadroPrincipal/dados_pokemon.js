91a363357ca2f48ee0ebcfa8fac1d25610f51b22
Atos Cunha <atos.silvacunha@gmail.com>
2025-08-23 15:06:40 -0300

    go

--- reacdex_frontend/src/componentes/QuadroPrincipal/dados_pokemon.js
+++
@@ -1,8 +0,0 @@
-export const itens = Array.from({ length: 10 }).map((_, index) => {
-  const id = index + 1;
-  return {
-    id,
-    nome: `pokemon_${id}`, 
-    src: `http://localhost:8000/home/${id}`
-  };
-});
--- reacdex_frontend/src/services/date_type.js
+++
@@ -1,8 +0,0 @@
-export const listPokemon = Array.from({ length: 10 }).map((_, index) => {
-  const nome = index + 1;
-  return {
-    id,
-    nome: `pokemon_${id}`, 
-    src: `http://localhost:8000/type/${id}`
-  };
-});
